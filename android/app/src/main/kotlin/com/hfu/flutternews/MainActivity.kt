53d4511ce5af76e98e18bf6929e13203ba7a8191
BHAUTIK PATEL <46004185+Bhautik004@users.noreply.github.com>
2020-09-18 09:03:25 +0530

    first

---
+++ android/app/src/main/kotlin/com/hfu/flutternews/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.hfu.flutternews
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/secret.dart
@@ -0,0 +1,1 @@
+String apiKey = "62ee60ad85c54d66af62ef78744f6580";
